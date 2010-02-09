7609f1290404b4988daafcb5c109c72700dfc185
Nathan Nelson <falken224@gmail.com>
2010-02-09 14:25:24 +0000

    Added message splitting. Refactored connector fire events.

---
+++ cat-ejb/ejbModule/com/digitalbarista/cat/message/event/connectorfire/MessageNodeFireHandler.java
@@ -0,0 +1,76 @@
+package com.digitalbarista.cat.message.event.connectorfire;
+
+import java.util.List;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+
+import com.digitalbarista.cat.business.CampaignMessagePart;
+import com.digitalbarista.cat.business.Connector;
+import com.digitalbarista.cat.business.MessageNode;
+import com.digitalbarista.cat.business.Node;
+import com.digitalbarista.cat.data.EntryPointType;
+import com.digitalbarista.cat.data.NodeDO;
+import com.digitalbarista.cat.data.SubscriberDO;
+import com.digitalbarista.cat.ejb.session.CampaignManager;
+import com.digitalbarista.cat.ejb.session.EventManager;
+import com.digitalbarista.cat.ejb.session.MessageManager;
+import com.digitalbarista.cat.message.event.CATEvent;
+
+public class MessageNodeFireHandler extends ConnectorFireHandler {
+
+	@Override
+	public void handle(EntityManager em, CampaignManager cMan, EventManager eMan, Connector conn, Node dest, Integer version, SubscriberDO s, CATEvent e) 
+	{
+		MessageNode mNode = (MessageNode)dest;
+		CATEvent sendMessageEvent=null;
+		NodeDO simpleNode=cMan.getSimpleNode(mNode.getUid());
+		
+		MessageManager mMan = null;
+		
+		try
+		{
+			InitialContext ic = new InitialContext();
+			mMan = (MessageManager)ic.lookup("ejb/cat/MessageManager");
+		}catch(NamingException ex)
+		{
+			throw new RuntimeException("Unable to retrieve the message manager.",ex);
+		}
+		
+		List<CampaignMessagePart> messageParts = mMan.getMessageParts(cMan.getDetailedCampaign(mNode.getCampaignUID()), mNode.getMessage());
+		
+		String fromAddress = s.getSubscriptions().get(simpleNode.getCampaign()).getLastHitEntryPoint();
+		EntryPointType fromType = s.getSubscriptions().get(simpleNode.getCampaign()).getLastHitEntryType();
+		for(CampaignMessagePart messagePart : messageParts)
+		{
+			if(messagePart.getEntryType()!=fromType)
+				continue;
+			for(String actualMessage : messagePart.getMessages())
+			{
+				switch(fromType)
+				{
+					
+					case Email:
+						sendMessageEvent = CATEvent.buildSendMessageRequestedEvent(fromAddress, fromType, s.getEmail(), actualMessage, mNode.getName(),mNode.getUid(),version);
+						break;
+					
+					case SMS:
+						sendMessageEvent = CATEvent.buildSendMessageRequestedEvent(fromAddress, fromType, s.getPhoneNumber(), actualMessage, mNode.getName(),mNode.getUid(),version);
+						break;
+						
+					case Twitter:
+						sendMessageEvent = CATEvent.buildSendMessageRequestedEvent(fromAddress, fromType, s.getTwitterUsername(), actualMessage, mNode.getName(),mNode.getUid(),version);
+						break;
+						
+					default:
+						throw new IllegalStateException("NodeDO must be either Email or SMS . . . mixed or other types are not supported.");
+				}
+			}
+		}
+		s.getSubscriptions().get(simpleNode.getCampaign()).setLastHitNode(simpleNode);
+		eMan.queueEvent(sendMessageEvent);
+		eMan.queueEvent(CATEvent.buildNodeOperationCompletedEvent(dest.getUid(), ""+s.getPrimaryKey()));
+	}
+
+}
---
+++ cat-ejb/ejbModule/com/digitalbarista/cat/message/event/connectorfire/TaggingNodeFireHandler.java
@@ -0,0 +1,69 @@
+package com.digitalbarista.cat.message.event.connectorfire;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+import com.digitalbarista.cat.business.Connector;
+import com.digitalbarista.cat.business.ContactTag;
+import com.digitalbarista.cat.business.Node;
+import com.digitalbarista.cat.business.TaggingNode;
+import com.digitalbarista.cat.data.ContactDO;
+import com.digitalbarista.cat.data.ContactTagDO;
+import com.digitalbarista.cat.data.EntryPointType;
+import com.digitalbarista.cat.data.NodeDO;
+import com.digitalbarista.cat.data.SubscriberDO;
+import com.digitalbarista.cat.ejb.session.CampaignManager;
+import com.digitalbarista.cat.ejb.session.EventManager;
+import com.digitalbarista.cat.message.event.CATEvent;
+
+public class TaggingNodeFireHandler extends ConnectorFireHandler {
+
+	@Override
+	public void handle(EntityManager em, CampaignManager cMan, EventManager eMan, Connector conn, Node dest, Integer version, SubscriberDO s, CATEvent e) {
+		TaggingNode tNode = (TaggingNode)dest;
+		NodeDO simpleNode=cMan.getSimpleNode(tNode.getUid());
+		List<ContactTagDO> tags = new ArrayList<ContactTagDO>();
+		for(ContactTag cTag : tNode.getTags())
+			tags.add(em.find(ContactTagDO.class, cTag.getContactTagId()));
+		ContactDO con;
+		Criteria crit = ((Session)em.getDelegate()).createCriteria(ContactDO.class);
+		EntryPointType ept = s.getSubscriptions().get(simpleNode.getCampaign()).getLastHitEntryType();
+		String address=null;
+		switch(ept)
+		{
+			case Email:
+				address=s.getEmail();
+				break;
+			case SMS:
+				address=s.getPhoneNumber();
+				break;
+			case Twitter:
+				address=s.getTwitterUsername();
+				break;
+		}
+		crit.add(Restrictions.eq("address",address));
+		crit.add(Restrictions.eq("type", ept));
+		crit.add(Restrictions.eq("client.id", simpleNode.getCampaign().getClient().getPrimaryKey()));
+		con = (ContactDO)crit.uniqueResult();
+		if(con==null)
+		{
+			con=new ContactDO();
+			con.setAddress(address);
+			con.setType(ept);
+			con.setCreateDate(Calendar.getInstance());
+			em.persist(con);
+		}
+		con.getContactTags().addAll(tags);
+		s.getSubscriptions().get(simpleNode.getCampaign()).setLastHitNode(simpleNode);
+		eMan.queueEvent(CATEvent.buildNodeOperationCompletedEvent(dest.getUid(), ""+s.getPrimaryKey()));
+
+	}
+
+}
