fa0be9867e7aa25e6dcf0a172025b91f36785584
Nathan Nelson <falken224@gmail.com>
2010-09-29 18:35:17 +0000

    trying to apply SOME sort of optimizations.

--- cat-ejb/ejbModule/com/digitalbarista/cat/ejb/session/UserManagerImpl.java
+++ cat-ejb/ejbModule/com/digitalbarista/cat/ejb/session/UserManagerImpl.java
@@ -139,7 +139,7 @@ public class UserManagerImpl implements UserManager {
 			Criteria crit = session.createCriteria(UserDO.class);
 			crit.add(Restrictions.eq("username", username));
 			crit.setCacheable(true);
-			crit.setCacheRegion("query/simpleUserByUsername");
+			crit.setCacheRegion("query/userByUsername");
 			UserDO ret = (UserDO)crit.uniqueResult();
 			
 			if(ret==null)
