181e3c2df818833f45cb35055cb6a78e29b0cb24
Nathan Nelson <falken224@gmail.com>
2011-12-07 17:28:40 +0000

    Fixing user update.

--- cat-ejb/ejbModule/com/digitalbarista/cat/ejb/session/UserManagerImpl.java
+++ cat-ejb/ejbModule/com/digitalbarista/cat/ejb/session/UserManagerImpl.java
@@ -284,14 +284,6 @@ public class UserManagerImpl implements UserManager {
 		} 
 		else 
 		{
-			// If NOT an admin and updating a password we need to verify the
-			// current password is correct
-			if (user.getPassword() != null &&
-					!ctx.isCallerInRole("admin") &&
-					!current.isCorrectPassword(user.getCurrentPassword()) )
-			{
-				throw new SecurityException("Current password did not match.  Cannot update user.");
-			}
 			user.copyTo(current);
 			if(user.getRoles()!=null)
 				syncRoles(user.getPrimaryKey(),new HashSet<Role>(user.getRoles()));
