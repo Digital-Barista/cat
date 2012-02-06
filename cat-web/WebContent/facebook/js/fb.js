7e321a282703cce8563802943d66641e950a8beb
Kris Hoyt <shirtpoker@gmail.com>
2012-02-06 01:13:21 +0000

    Reformatted fb.js and added getting and deleting app requests

--- cat-web/WebContent/facebook/js/fb.js
+++ cat-web/WebContent/facebook/js/fb.js
@@ -1,105 +1,144 @@
-var channel_path = '/cat/facebook/xd_receiver.htm'; 
-var currentUID;
+dbi = this.dbi || {};
 
-function fbInit()
-{
-	var appId = document.getElementById('appId').value;
-	
-	// Initialize facebook API
-	FB.init({appId: appId, status: true, cookie: true, xfbml: true});
+$.extend(dbi, {
+  
+  channel_path: '/cat/facebook/xd_receiver.htm',
+  currentUID: undefined,
 
-	// Set canvas size
-	FB.Canvas.setSize();
-
-	// Listen for session change events
-	FB.Event.subscribe('auth.sessionChange', function(response) {
-		handleResponse(response);
-	});
-	
-	// Check the login status
-	FB.getLoginStatus(function(response) {
-		handleResponse(response);
-	});
-	
-	// Wire events
-	if ($("#InviteButton") &&
-		$("#Invite") )
-	{
-		$("#InviteButton").click(function(){
-			if ($("#Invite").css("display") == "block")
-				$("#Invite").css("display", "none");
-			else
-				$("#Invite").css("display", "block");
-			
-			FB.Canvas.setSize();
-		});
-	}
-}
-function handleResponse(response)
-{
-	if (response.authResponse ||
-		response.session) 
-	  {
-		if (response.session){
-			currentUID = response.session.uid;
-		}
-		else{
-			currentUID = response.authResponse.userID;
-		}
-		  hideLogin();
-		  fetchMessages();
-		  showBookmark();
+  fbInit: function() {
+  	var appId = document.getElementById('appId').value;
+  	
+  	// Initialize facebook API
+  	FB.init({appId: appId, status: true, cookie: true, xfbml: true});
+  
+  	// Set canvas size
+  	FB.Canvas.setSize();
+  
+  	// Listen for session change events
+  	FB.Event.subscribe('auth.sessionChange', function(response) {
+  	  dbi.handleResponse(response);
+  	});
+  	
+  	// Check the login status
+  	FB.getLoginStatus(function(response) {
+  	  dbi.handleResponse(response);
+  	});
+  	
+  	// Wire events
+  	if ($("#InviteButton") &&
+  		$("#Invite") )
+  	{
+  		$("#InviteButton").click(function(){
+  			if ($("#Invite").css("display") == "block")
+  				$("#Invite").css("display", "none");
+  			else
+  				$("#Invite").css("display", "block");
+  			
+  			FB.Canvas.setSize();
+  		});
+  	}
+  },
+  
+  handleResponse: function(response){
+  	if (response.authResponse ||
+  		response.session) 
+  	{
+  	  // Clear all app requests
+  	  dbi.deleteAllAppRequests();
+  	  
+  	  // Get UID from response
+  		if (response.session){
+  		  dbi.currentUID = response.session.uid;
+  		}
+  		else{
+  		  dbi.currentUID = response.authResponse.userID;
+  		}
+  		
+  		dbi.hideLogin();
+  		dbi.fetchMessages();
+  		dbi.showBookmark();
 		  
 		  FB.Canvas.setSize();
 	  } 
 	  else 
 	  {
-		  showLogin();
-		  hideBookmark();
+	    dbi.showLogin();
+	    dbi.hideBookmark();
 	  }
-}
-function showLogin()
-{
-	$("#LoadingMessage").css("display", "none");
-	$("#Login").css("display", "block");
-	$("#MessageArea").css("display", "none");
-}
-function hideLogin()
-{
-	$("#Login").css("display", "none");
-	$("#MessageArea").css("display", "block");
-}
-function showBookmark()
-{
-	$("#Bookmark").css("display", "block");
-}
-function hideBookmark()
-{
-	$("#Bookmark").css("display", "none");
-}
+  },
+  
+  showLogin: function(){
+  	$("#LoadingMessage").css("display", "none");
+  	$("#Login").css("display", "block");
+  	$("#MessageArea").css("display", "none");
+  },
+  
+  hideLogin: function(){
+  	$("#Login").css("display", "none");
+  	$("#MessageArea").css("display", "block");
+  },
+  
+  showBookmark: function(){
+  	$("#Bookmark").css("display", "block");
+  },
+  
+  hideBookmark: function(){
+  	$("#Bookmark").css("display", "none");
+  },
+  
+  fetchMessages: function(){
+  	var messageApi = new MessageAPI();
+  	messageApi.loadMessages();
+  },
 
-function fetchMessages()
-{
-	var messageApi = new MessageAPI();
-	messageApi.loadMessages();
-	
-}
+  fetchQueryParameter: function(name){
+  	var query = window.location.search;
+  	
+  	var parts = query.split("&");
+  	for (var i = 0; i < parts.length; i++)
+  	{
+  		var param = parts[i].split("=");
+  		if (param.length == 2 &&
+  			param[0] == name)
+  		{
+  			return param[1];
+  		}
+  	}
+  	return null;
+  },
+  
+  getAppRequests: function(callback){
+    FB.api('/me/apprequests', function(result){
+      if (callback){
+        callback(result);
+      }
+    });
+  },
+  
+  deleteAppRequest: function(id, callback){
+    FB.api('/' + id, 'DELETE', function(result){
+      if (callback){
+        callback(result);
+      }
+    });
+  },
+  
+  deleteAllAppRequests: function(){
+    dbi.getAppRequests(function(result){
+      var i, L,
+          data;
+      if (result && result.data){
+        data = result.data;
+        for (i = 0, L = data.length; i < L; i++){
+          dbi.deleteAppRequest(data[i].id);
+        }
+      }
+    });
+  }
+  
+});
 
-function fetchQueryParameter(name)
-{
-	var query = window.location.search;
-	
-	var parts = query.split("&");
-	for (var i = 0; i < parts.length; i++)
-	{
-		var param = parts[i].split("=");
-		if (param.length == 2 &&
-			param[0] == name)
-		{
-			return param[1];
-		}
-	}
-	return null;
-}
-
+$(document).ready(function(){
+  dbi.fbInit();
+});
 
