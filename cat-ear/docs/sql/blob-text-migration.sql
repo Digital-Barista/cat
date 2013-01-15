144ae4004fb7e8b0fdc0e25b0738c4f39d21e487
Nathan Nelson <falken224@gmail.com>
2013-01-15 05:30:10 +0000

    Removing the EAR project.

--- cat-ear/docs/sql/blob-text-migration.sql
+++
@@ -1,3 +0,0 @@
-create table audit_generic_data(audit_id bigint primary key, audit_data text);
-insert into audit_generic_data(audit_id,audit_data) (select audit_id, audit_data from audit_generic);
-alter table audit_generic drop column audit_data;
